a00a6869854c7c3cca8fba7d17df79632c184903
swativishwakarma <157468544+swativishwakarma@users.noreply.github.com>
2024-07-24 21:07:22 +0530

    Update PreparationTime.java

--- java/java_prgs_using_sacnner/PreparationTime.java
+++ java/java_prgs_using_sacnner/PreparationTime.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class PreparationTime {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
         int N= sc.nextInt();
         System.out.print(N*2);
 
